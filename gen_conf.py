1691a05286018483f0aeb3f46247db4c73476c1b
Rodrigo <dev@brunneis.com>
2019-10-30 21:12:56 +0100

    Add support for SSL backends

--- gen_conf.py
+++ gen_conf.py
@@ -14,7 +14,13 @@ def load_haproxy_conf():
 
 def load_domains_conf():
     with open('domains.yaml', 'r') as yaml_file:
-        return yaml.load(yaml_file)
+        conf = yaml.load(yaml_file)
+        for key, value in conf.items():
+            if 'endpoints' not in value:
+                value['endpoints'] = []
+            if 'ssl_endpoints' not in value:
+                value['ssl_endpoints'] = []
+        return conf
 
 
 def get_inbound_template():
@@ -30,8 +36,7 @@ def get_inbound_template():
 
 
 def should_print_empty_line(key, value, already_printed):
-    if len(value) > 1 and key not in ['option', 'server'
-                                      ] and not already_printed:
+    if len(value) > 1 and key not in ['option', 'server'] and not already_printed:
         return True
     return False
 
@@ -70,30 +75,38 @@ def update_haproxy_conf_with_domains(haproxy_conf, domains_conf):
     non_ssl_domains = []
     for i, kv in enumerate(domains_conf.items()):
         key, value = kv
-        domains_string = ' '.join(
-            [f'-i {domain}' for domain in value['domains']])
-        inbound_template['frontend inbound']['acl'].append(
-            f'is_domain{i} hdr(host) {domains_string}')
+        domains_string = ' '.join([f'-i {domain}' for domain in value['domains']])
+        inbound_template['frontend inbound']['acl'].append(f'is_domain{i} hdr(host) {domains_string}')
+        
         if 'ssl' in value and not value['ssl']:
             non_ssl_domains.append(i)
-        inbound_template['frontend inbound']['use_backend'].append(
-            f'domain{i} if is_domain{i}')
+        inbound_template['frontend inbound']['use_backend'].append(f'domain{i} if is_domain{i}')
+        
         server_lines = []
-        check_string = ''
-        if len(value['endpoints']) > 1:
-            check_string = 'check '
-        for j, endpoint in enumerate(value['endpoints']):
-            server_lines.append(
-                f"s{i}{j} {endpoint} {check_string}maxconn {haproxy_conf['global']['maxconn']}"
-            )
+
+        if 'endpoints' in value:
+            check_string = ''
+            if len(value['endpoints']) > 1:
+                check_string = 'check '
+            for j, endpoint in enumerate(value['endpoints']):
+                server_lines.append(f"s{i}{j} {endpoint} {check_string}maxconn {haproxy_conf['global']['maxconn']}")
+
+        if 'ssl_endpoints' in value:
+            ssl_check_string = ''
+            if len(value['ssl_endpoints']) > 1:
+                ssl_check_string = 'check '
+            for j, ssl_endpoint in enumerate(value['ssl_endpoints']):
+                server_lines.append(
+                    f"s{i}{j} {ssl_endpoint} {ssl_check_string}ssl verify none maxconn {haproxy_conf['global']['maxconn']}"
+                )
+
         inbound_template[f'backend domain{i} # {key}'] = {
             'balance': 'roundrobin',
             'option': ['httpclose', 'forwardfor'],
             'server': server_lines
         }
     neg_string = ' '.join([f'!is_domain{i}' for i in non_ssl_domains])
-    inbound_template['frontend inbound'][
-        'redirect'] = f"scheme https code 301 if {neg_string} !{{ ssl_fc }}"
+    inbound_template['frontend inbound']['redirect'] = f"scheme https code 301 if {neg_string} !{{ ssl_fc }}"
     haproxy_conf.update(inbound_template)
 
 
@@ -106,7 +119,9 @@ def dump_certbot_script(domains_conf):
         output_file.write('#!/bin/bash\n')
         certonly_base_cmd = f"certbot certonly --non-interactive --keep --expand --agree-tos -m {os.environ['LETSENCRYPT_EMAIL']} --standalone -d"
         for domain in domains:
-            output_file.write(f'{certonly_base_cmd} {domain} && cat /etc/letsencrypt/live/{domain}/fullchain.pem /etc/letsencrypt/live/{domain}/privkey.pem > /etc/haproxy/ssl/{domain}.pem\n')
+            output_file.write(
+                f'{certonly_base_cmd} {domain} && cat /etc/letsencrypt/live/{domain}/fullchain.pem /etc/letsencrypt/live/{domain}/privkey.pem > /etc/haproxy/ssl/{domain}.pem\n'
+            )
 
 
 if __name__ == "__main__":
