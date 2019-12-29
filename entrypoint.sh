3b3e5f289527bf4a50600ab24cd9cf2117422110
Rodrigo <dev@brunneis.com>
2019-12-29 18:51:29 +0100

    Minor changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,6 +11,7 @@ function request_certs {
     result=$(sha1sum -c get-certs.sh.sha1 | cut -d: -f 2 | xargs 2>/dev/null)
     exec 2>&3
     if [ "$result" != "OK" ]; then
+      echo "* Changes detected"
       bash get-certs.sh
     fi
   fi
@@ -18,24 +19,35 @@ function request_certs {
 }
 
 function start_haproxy {
-  haproxy -db -p /var/run/haproxy.pid -f haproxy.cfg 
+  echo -e "\n * Starting HAProxy"
+  haproxy -D -p /var/run/haproxy.pid -f haproxy.cfg
+  echo -e "   ...done.\n"
 }
 
 function stop_haproxy {
+  echo " * Stopping HAProxy"
 	for pid in $(cat /var/run/haproxy.pid); do
     kill $pid
 	done
 	rm -f /var/run/haproxy.pid
+  echo -e "   ...done.\n"
 }
+
+function renew_certs {
+  certbot renew --standalone
+}
+
+mkdir -p /opt/haproxy/ssl
 
 python3 gen_conf.py
 request_certs
-mkdir -p /opt/haproxy/ssl
+
+cat haproxy.cfg
 
 while true; do
-  certbot renew --standalone
   bash load-certs.sh
   start_haproxy
   sleep 86400 # 1 day
   stop_haproxy
+  renew_certs
 done
